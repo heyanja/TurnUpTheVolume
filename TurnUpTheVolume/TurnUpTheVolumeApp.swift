85694b825e2ca1b8fd0dc99496be623e025d4620
Anna <119071299+heyanja@users.noreply.github.com>
2024-05-22 12:48:10 -0400

    Initial Commit

---
+++ TurnUpTheVolume/TurnUpTheVolumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurnUpTheVolumeApp.swift
+//  TurnUpTheVolume
+//
+//  Created by Anna Fadieieva on 2024-05-22.
+//
+
+import SwiftUI
+
+@main
+struct TurnUpTheVolumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
